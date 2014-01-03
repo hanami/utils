5e446b3348b216042017993b4ebe116a70d44fc8
Luca Guidi <me@lucaguidi.com>
2014-01-03 11:02:43 +0100

    SemVer: initial release SHOULD be 0.1.0

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Lotus::Utils::VERSION do
   it 'exposes version' do
-    Lotus::Utils::VERSION.must_equal '0.0.1'
+    Lotus::Utils::VERSION.must_equal '0.1.0'
   end
 end
