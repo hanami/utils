c765b82daa1514213fdfb128b3e725bdc04e6e87
Luca Guidi <me@lucaguidi.com>
2014-01-06 18:08:33 +0100

    Added test coverage reporter

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 gemspec
 
-gem 'debugger', require: false
-gem 'yard',     require: false
+gem 'debugger',  require: false
+gem 'yard',      require: false
+gem 'simplecov', require: false
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,13 @@
 $:.unshift 'lib'
+
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter 'test'
+    command_name 'Mintest'
+  end
+end
+
 require 'rubygems'
 require 'bundler/setup'
 require 'minitest/autorun'
