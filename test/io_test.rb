5fc5b1c3251e44a8407021f925c223114ac2ed7d
Luca Guidi <me@lucaguidi.com>
2014-01-23 12:08:43 +0100

    Skip Lotus::Utils::IO test on unsupported environments

--- test/io_test.rb
+++ test/io_test.rb
@@ -8,14 +8,22 @@ end
 describe Lotus::Utils::IO do
   describe '.silence_warnings' do
     it 'lowers verbosity of stdout' do
-      position = STDOUT.tell
+      begin
+        position = STDOUT.tell
 
-      Lotus::Utils::IO.silence_warnings do
-        IOTest::TEST_CONSTANT = 'redefined'
+        Lotus::Utils::IO.silence_warnings do
+          IOTest::TEST_CONSTANT = 'redefined'
+        end
+
+        IOTest::TEST_CONSTANT.must_equal('redefined')
+        STDOUT.tell.must_equal(position)
+      rescue Errno::ESPIPE
+        puts 'Skipping this test, IO#tell is not supported in this environment'
+
+        Lotus::Utils::IO.silence_warnings do
+          IOTest::TEST_CONSTANT = 'redefined'
+        end
       end
-
-      IOTest::TEST_CONSTANT.must_equal('redefined')
-      STDOUT.tell.must_equal(position)
     end
   end
 end
