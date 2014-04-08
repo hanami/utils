e1a35e26a72af126de579e14c386433297ae64f0
Luca Guidi <me@lucaguidi.com>
2014-04-08 11:09:19 +0200

    Implemented Lotus::Utils::Kernel.Integer

--- lib/lotus/utils/kernel.rb
+++ lib/lotus/utils/kernel.rb
@@ -3,7 +3,7 @@ module Lotus
     # Kernel utilities
     # @since 0.1.1
     module Kernel
-      # Coerces an the argument to be an array.
+      # Coerces the argument to be an array.
       #
       # It's similar to Ruby's Kernel.Array, but it applies further
       # transformations:
@@ -35,6 +35,96 @@ module Lotus
       #   Lotus::Utils::Kernel.Array([1, [2, nil, 1]]) # => [1,2]
       def self.Array(arg)
         super(arg).flatten.compact.uniq
+      end
+
+      # Coerces the argument to be an integer.
+      #
+      # It's similar to Ruby's Kernel.Integer, but it doesn't stop at the first
+      # error and tries to be less "whiny".
+      #
+      # @param arg [Object] the argument
+      #
+      # @return [Fixnum,nil] the result of the coercion
+      #
+      # @raise [TypeError,FloatDomainError,RangeError] if the argument can't be
+      #   coerced or if it's too big.
+      #
+      # @since 0.1.1
+      #
+      # @see http://www.ruby-doc.org/core-2.1.1/Kernel.html#method-i-Integer
+      #
+      # @example Basic Usage
+      #   require 'bigdecimal'
+      #   require 'lotus/utils/kernel'
+      #
+      #   Lotus::Utils::Kernel.Integer(1)                        # => 1
+      #   Lotus::Utils::Kernel.Integer(1.2)                      # => 1
+      #   Lotus::Utils::Kernel.Integer("1")                      # => 1
+      #   Lotus::Utils::Kernel.Integer(Rational(0.3))            # => 0
+      #   Lotus::Utils::Kernel.Integer(Complex(0.3))             # => 0
+      #   Lotus::Utils::Kernel.Integer(BigDecimal.new(12.00001)) # => 12
+      #   Lotus::Utils::Kernel.Integer(Time.now)                 # => 1396947161
+      #   Lotus::Utils::Kernel.Integer(176605528590345446089)
+      #     # => 176605528590345446089
+      #
+      # @example Integer Interface
+      #   require 'lotus/utils/kernel'
+      #
+      #   UltimateAnswer = Struct.new(:question) do
+      #     def to_int
+      #       42
+      #     end
+      #   end
+      #
+      #   answer = UltimateAnswer.new('The Ultimate Question of Life')
+      #   Lotus::Utils::Kernel.Integer(answer) # => 42
+      #
+      # @example Error Handling
+      #   require 'lotus/utils/kernel'
+      #
+      #   # nil
+      #   Kernel.Integer(nil)               # => TypeError
+      #   Lotus::Utils::Kernel.Integer(nil) # => nil
+      #
+      #   # float represented as a string
+      #   Kernel.Integer("23.4")               # => ArgumentError
+      #   Lotus::Utils::Kernel.Integer("23.4") # => 23
+      #
+      #   # rational represented as a string
+      #   Kernel.Integer("2/3")               # => ArgumentError
+      #   Lotus::Utils::Kernel.Integer("2/3") # => 2
+      #
+      #   # complex represented as a string
+      #   Kernel.Integer("2.5/1")               # => ArgumentError
+      #   Lotus::Utils::Kernel.Integer("2.5/1") # => 2
+      #
+      # @example Unchecked Exceptions
+      #   require 'bigdecimal'
+      #   require 'lotus/utils/kernel'
+      #
+      #   # Missing #to_int and #to_i
+      #   input = OpenStruct.new(color: 'purple')
+      #   Lotus::Utils::Kernel.Integer(input) # => TypeError
+      #
+      #   # bigdecimal infinity
+      #   input = BigDecimal.new("Infinity")
+      #   Lotus::Utils::Kernel.Integer(input) # => FloatDomainError
+      #
+      #   # bigdecimal NaN
+      #   input = BigDecimal.new("NaN")
+      #   Lotus::Utils::Kernel.Integer(input) # => FloatDomainError
+      #
+      #   # big rational
+      #   input = Rational(-8) ** Rational(1, 3)
+      #   Lotus::Utils::Kernel.Integer(input) # => RangeError
+      #
+      #   # big complex represented as a string
+      #   input = Complex(2, 3)
+      #   Lotus::Utils::Kernel.Integer(input) # => RangeError
+      def self.Integer(arg)
+        super(arg) if arg
+      rescue ArgumentError
+        arg.to_i
       end
     end
   end
--- test/kernel_test.rb
+++ test/kernel_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'bigdecimal'
 require 'lotus/utils/kernel'
 
 describe Lotus::Utils::Kernel do
@@ -52,6 +53,170 @@ describe Lotus::Utils::Kernel do
 
       it 'returns an array with uniq values' do
         @result.must_equal [2,3]
+      end
+    end
+  end
+
+  describe '.Integer' do
+    describe 'successful operations' do
+      before do
+        PersonFavNumber = Struct.new(:name) do
+          def to_int
+            23
+          end
+        end
+
+        @result = Lotus::Utils::Kernel.Integer(input)
+      end
+
+      after do
+        Object.send(:remove_const, :PersonFavNumber)
+      end
+
+      describe 'when nil is given' do
+        let(:input) { nil }
+
+        it 'returns nil' do
+          @result.must_equal nil
+        end
+      end
+
+      describe 'when a fixnum given' do
+        let(:input) { 1 }
+
+        it 'returns an integer' do
+          @result.must_equal 1
+        end
+      end
+
+      describe 'when a float is given' do
+        let(:input) { 1.2 }
+
+        it 'returns an integer' do
+          @result.must_equal 1
+        end
+      end
+
+      describe 'when a string given' do
+        let(:input) { '23' }
+
+        it 'returns an integer' do
+          @result.must_equal 23
+        end
+      end
+
+      describe 'when a string representing a float number is given' do
+        let(:input) { '23.4' }
+
+        it 'returns an integer' do
+          @result.must_equal 23
+        end
+      end
+
+      describe 'when a bignum is given' do
+        let(:input) { 13289301283 ** 2 }
+
+        it 'returns an bignum' do
+          @result.must_equal 176605528590345446089
+        end
+      end
+
+      describe 'when a bigdecimal is given' do
+        let(:input) { BigDecimal.new('12.0001') }
+
+        it 'returns an bignum' do
+          @result.must_equal 12
+        end
+      end
+
+      describe 'when a complex number is given' do
+        let(:input) { Complex(0.3) }
+
+        it 'returns an integer' do
+          @result.must_equal 0
+        end
+      end
+
+      describe 'when a string representing a complex number is given' do
+        let(:input) { '2.5/1' }
+
+        it 'returns an integer' do
+          @result.must_equal 2
+        end
+      end
+
+      describe 'when a rational number is given' do
+        let(:input) { Rational(0.3) }
+
+        it 'returns an integer' do
+          @result.must_equal 0
+        end
+      end
+
+      describe 'when a string representing a rational number is given' do
+        let(:input) { '2/3' }
+
+        it 'returns an integer' do
+          @result.must_equal 2
+        end
+      end
+
+      describe 'when an object that implements #to_i is given' do
+        let(:input) { Time.at(0) }
+
+        it 'returns an integer' do
+          @result.must_equal 0
+        end
+      end
+
+      describe 'when an object that implements #to_int is given' do
+        let(:input) { PersonFavNumber.new('Luca') }
+
+        it 'returns an integer' do
+          @result.must_equal 23
+        end
+      end
+    end
+
+    describe 'failure operations' do
+      describe "when a an object that doesn't implement any integer interface" do
+        let(:input) { OpenStruct.new(color: 'purple') }
+
+        it 'raises error' do
+          -> { Lotus::Utils::Kernel.Integer(input) }.must_raise(TypeError)
+        end
+      end
+
+      describe 'when a bigdecimal infinity is given' do
+        let(:input) { BigDecimal.new('Infinity') }
+
+        it 'raises error' do
+          -> { Lotus::Utils::Kernel.Integer(input) }.must_raise(FloatDomainError)
+        end
+      end
+
+      describe 'when a bigdecimal NaN is given' do
+        let(:input) { BigDecimal.new('NaN') }
+
+        it 'raises error' do
+          -> { Lotus::Utils::Kernel.Integer(input) }.must_raise(FloatDomainError)
+        end
+      end
+
+      describe 'when a big complex number is given' do
+        let(:input) { Complex(2,3) }
+
+        it 'raises error' do
+          -> { Lotus::Utils::Kernel.Integer(input) }.must_raise(RangeError)
+        end
+      end
+
+      describe 'when a big rational number is given' do
+        let(:input) { Rational(-8) ** Rational(1, 3) }
+
+        it 'raises error' do
+          -> { Lotus::Utils::Kernel.Integer(input) }.must_raise(RangeError)
+        end
       end
     end
   end
