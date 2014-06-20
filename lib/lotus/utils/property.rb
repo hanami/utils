84b9248a90f2ea858c76b16edfb2bee66cec9dce
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2014-06-20 21:47:32 +0200

    Lotus::Utils::Property subclass Module for fun an profit
    * Add Lotus#property helper method

--- lib/lotus/utils/property.rb
+++ lib/lotus/utils/property.rb
@@ -1,23 +1,32 @@
 module Lotus
-  module Utils
-    module Property
-      def self.included(base)
-        base.class_eval do
-          extend ClassMethods
-        end
+  def self.property(name, &blk)
+    Lotus::Utils::Property.new(name, &blk)
+  end
+end
+
+module Lotus  
+  module Utils    
+    class Property < Module
+      def initialize(name, &blk)
+        @name, @blk = name, blk
       end
       
-      module ClassMethods
-        def property(name, &blk)
-          define_method name do |val = nil|
-            if val
-              instance_variable_set("@#{name}", val)
-            else
-              instance_eval &blk
-            end
+      def included(base)
+        super
+        define_property(@name, &@blk)
+      end
+      
+      private
+      
+      def define_property(name, &blk)
+        define_method(name) do |val = nil|
+          if val
+            instance_variable_set("@#{name}", val)
+          else
+            instance_eval &blk
           end
         end
       end
-    end
+    end    
   end
 end
--- test/property_test.rb
+++ test/property_test.rb
@@ -1,15 +1,28 @@
 require 'test_helper'
 require 'lotus/utils/property'
 
-class Property
-  include Lotus::Utils::Property
-  
-  property(:word) { @word.capitalize }
+class MethodProperty
+  include Lotus.property(:word) { @word.capitalize }
+end
+
+class ModuleProperty
+  include Lotus::Utils::Property.new(:word) { @word.capitalize }
 end
 
 describe Lotus::Utils::Property do
   before do
-    @property = Property.new
+    @property = MethodProperty.new
+  end
+  
+  it 'sets instance variable in writer mode and calls block in reader mode' do
+    @property.word('hello')
+    @property.word.must_equal 'Hello'
+  end
+end
+
+describe Lotus::Utils::Property do
+  before do
+    @property = ModuleProperty.new
   end
   
   it 'sets instance variable in writer mode and calls block in reader mode' do
