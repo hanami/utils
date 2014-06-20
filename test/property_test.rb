8d49c1dde1fb26e49dce62141c64f32f52a00ea9
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2014-06-21 01:17:36 +0200

    Revert "Lotus::Utils::Property subclass Module for fun an profit"
    This reverts commit 84b9248a90f2ea858c76b16edfb2bee66cec9dce.

--- test/property_test.rb
+++ test/property_test.rb
@@ -1,28 +1,15 @@
 require 'test_helper'
 require 'lotus/utils/property'
 
-class MethodProperty
-  include Lotus.property(:word) { @word.capitalize }
-end
-
-class ModuleProperty
-  include Lotus::Utils::Property.new(:word) { @word.capitalize }
-end
-
-describe Lotus::Utils::Property do
-  before do
-    @property = MethodProperty.new
-  end
+class Property
+  include Lotus::Utils::Property
   
-  it 'sets instance variable in writer mode and calls block in reader mode' do
-    @property.word('hello')
-    @property.word.must_equal 'Hello'
-  end
+  property(:word) { @word.capitalize }
 end
 
 describe Lotus::Utils::Property do
   before do
-    @property = ModuleProperty.new
+    @property = Property.new
   end
   
   it 'sets instance variable in writer mode and calls block in reader mode' do
