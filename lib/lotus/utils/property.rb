2c9b98b4c070fdf080a977fcffa3d9e928e216c5
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2014-07-06 12:16:45 +0200

    Add caching of property's block evaluation

--- lib/lotus/utils/property.rb
+++ lib/lotus/utils/property.rb
@@ -7,11 +7,14 @@ module Lotus
       
       module ClassMethods
         def property(name, &blk)
+          ivar_name        = "@#{name}"
+          ivar_name_cached = "#{ivar_name}_cached"
+
           define_method name do |val = nil|
             if val
-              instance_variable_set("@#{name}", val)
+              instance_variable_set(ivar_name, val)
             else
-              instance_eval &blk
+              instance_variable_get(ivar_name_cached) || instance_variable_set(ivar_name_cached, instance_eval(&blk))
             end
           end
         end
--- test/property_test.rb
+++ test/property_test.rb
@@ -3,17 +3,38 @@ require 'lotus/utils/property'
 
 class Property
   include Lotus::Utils::Property
-  
-  property(:word) { @word.capitalize }
+
+  property(:fruit) { @fruit.capitalize }
+
+  def other_fruit=(fruit)
+    @fruit = fruit
+  end
 end
 
 describe Lotus::Utils::Property do
   before do
     @property = Property.new
   end
-  
-  it 'sets instance variable in writer mode and calls block in reader mode' do
-    @property.word('hello')
-    @property.word.must_equal 'Hello'
+
+  describe 'defined with block' do
+    it 'sets instance variable in writer mode and calls block in reader mode' do
+      @property.fruit('orange')
+      @property.fruit.must_equal 'Orange'
+    end
+
+    describe 'instance variable already set' do
+      before do
+        @property.other_fruit = 'banana'
+      end
+
+      it 'calls block in reader mode' do
+        @property.fruit.must_equal 'Banana'
+      end
+
+      it 'sets instance variable in writer mode' do
+        @property.fruit('apple')
+        @property.fruit.must_equal 'Apple'
+      end
+    end
   end
 end
