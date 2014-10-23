79a3cd1e5ac5f4619d559756bdead3a70b20c710
Luca Guidi <me@lucaguidi.com>
2014-10-23 09:22:45 +0200

    Prepare for v0.3.0

--- lib/lotus/utils/version.rb
+++ lib/lotus/utils/version.rb
@@ -1,5 +1,5 @@
 module Lotus
   module Utils
-    VERSION = '0.2.0'
+    VERSION = '0.3.0'.freeze
   end
 end
--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Lotus::Utils::VERSION do
   it 'exposes version' do
-    Lotus::Utils::VERSION.must_equal '0.2.0'
+    Lotus::Utils::VERSION.must_equal '0.3.0'
   end
 end
