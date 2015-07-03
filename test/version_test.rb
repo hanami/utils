a25b6f409d651e10a824a547553994518c0f9321
Luca Guidi <me@lucaguidi.com>
2015-07-03 09:54:05 +0200

    Bump to v0.5.1

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Lotus::Utils::VERSION do
   it 'exposes version' do
-    Lotus::Utils::VERSION.must_equal '0.5.0'
+    Lotus::Utils::VERSION.must_equal '0.5.1'
   end
 end
