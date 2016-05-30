1159acfae6bc0e463c106afd6c78e5b5a3c8be39
Luca Guidi <me@lucaguidi.com>
2016-05-30 16:38:42 +0200

    Extend with tests `Hanami::Utils::Json` Ref #142

--- lib/hanami/utils/json.rb
+++ lib/hanami/utils/json.rb
@@ -12,11 +12,40 @@ module Hanami
     # Otherwise - JSON std lib.
     #
     # @since x.x.x
-    if defined? MultiJson
-      Json = MultiJson
-      Json::ParserError = MultiJson::ParseError
-    else
-      Json = ::JSON
+    module Json
+      # rubocop:disable Style/ClassVars
+      if defined?(MultiJson)
+        @@engine    = MultiJson
+        ParserError = MultiJson::ParseError
+      else
+        @@engine    = ::JSON
+        ParserError = ::JSON::ParserError
+      end
+      # rubocop:enable Style/ClassVars
+
+      # Load the given JSON payload into Ruby objects.
+      #
+      # @param payload [String] a JSON payload
+      #
+      # @return [Object] the result of the loading process
+      #
+      # @raise [Hanami::Utils::Json::ParserError] if the paylod is invalid
+      #
+      # @since x.x.x
+      def self.load(payload)
+        @@engine.load(payload)
+      end
+
+      # Dump the given object into a JSON payload
+      #
+      # @param object [Object] any object
+      #
+      # @return [String] the result of the dumping process
+      #
+      # @since x.x.x
+      def self.dump(object)
+        @@engine.dump(object)
+      end
     end
   end
 end
