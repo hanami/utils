895f109f672e4084493f4011128fb6bcf94660bd
Luca Guidi <me@lucaguidi.com>
2016-11-17 13:31:54 +0100

    Introduced `Hanami::Utils::Json.parse` for safe JSON parsing. Deprecate `Hanami::Utils::Json.load` in favor of `.parse`.

--- lib/hanami/utils/json.rb
+++ lib/hanami/utils/json.rb
@@ -4,6 +4,8 @@ rescue LoadError
   require 'json'
 end
 
+require 'hanami/utils/deprecation'
+
 module Hanami
   module Utils
     # JSON wrapper
@@ -13,9 +15,33 @@ module Hanami
     #
     # @since 0.8.0
     module Json
+      # MultiJson adapter
+      #
+      # @since 0.9.1
+      # @api private
+      class MultiJsonAdapter
+        # @since 0.9.1
+        # @api private
+        def parse(payload)
+          MultiJson.load(payload)
+        end
+
+        # FIXME: remove this alias, when Hanami::Utils::Json.load will be removed
+        #
+        # @since 0.9.1
+        # @api private
+        alias load parse
+
+        # @since 0.9.1
+        # @api private
+        def dump(object)
+          MultiJson.dump(object)
+        end
+      end
+
       # rubocop:disable Style/ClassVars
       if defined?(MultiJson)
-        @@engine    = MultiJson
+        @@engine    = MultiJsonAdapter.new
         ParserError = MultiJson::ParseError
       else
         @@engine    = ::JSON
@@ -32,8 +58,24 @@ module Hanami
       # @raise [Hanami::Utils::Json::ParserError] if the paylod is invalid
       #
       # @since 0.8.0
+      #
+      # @deprecated Use {#parse} instead
       def self.load(payload)
+        Hanami::Utils::Deprecation.new("`Hanami::Utils::Json.load' is deprecated, please use `Hanami::Utils::Json.parse'")
         @@engine.load(payload)
+      end
+
+      # Parse the given JSON paylod
+      #
+      # @param payload [String] a JSON payload
+      #
+      # @return [Object] the result of the loading process
+      #
+      # @raise [Hanami::Utils::Json::ParserError] if the paylod is invalid
+      #
+      # @since 0.9.1
+      def self.parse(payload)
+        @@engine.parse(payload)
       end
 
       # Dump the given object into a JSON payload
--- test/isolation/json/multi_json_test.rb
+++ test/isolation/json/multi_json_test.rb
@@ -11,17 +11,46 @@ require 'hanami/utils/json'
 describe Hanami::Utils::Json do
   describe 'with MultiJson' do
     it 'uses MultiJson engine' do
-      Hanami::Utils::Json.class_variable_get(:@@engine).must_equal(MultiJson)
+      Hanami::Utils::Json.class_variable_get(:@@engine).must_be_kind_of(Hanami::Utils::Json::MultiJsonAdapter)
     end
 
     describe '.load' do
       it 'loads given payload' do
-        actual = Hanami::Utils::Json.load %({"a":1})
+        capture_io do
+          actual = Hanami::Utils::Json.load %({"a":1})
+          actual.must_equal('a' => 1)
+        end
+      end
+
+      it 'raises error if given payload is malformed' do
+        capture_io do
+          -> { Hanami::Utils::Json.load %({"a:1}) }.must_raise(Hanami::Utils::Json::ParserError)
+        end
+      end
+
+      it 'is deprecated' do
+        _, err = capture_io do
+          Hanami::Utils::Json.load %({"a":1})
+        end
+
+        err.must_include "`Hanami::Utils::Json.load' is deprecated, please use `Hanami::Utils::Json.parse'"
+      end
+    end
+
+    describe '.parse' do
+      it 'loads given payload' do
+        actual = Hanami::Utils::Json.parse %({"a":1})
         actual.must_equal('a' => 1)
       end
 
       it 'raises error if given payload is malformed' do
-        -> { Hanami::Utils::Json.load %({"a:1}) }.must_raise(Hanami::Utils::Json::ParserError)
+        -> { Hanami::Utils::Json.parse %({"a:1}) }.must_raise(Hanami::Utils::Json::ParserError)
+      end
+
+      # See: https://github.com/hanami/utils/issues/169
+      it "doesn't eval payload" do
+        actual = Hanami::Utils::Json.parse %({"json_class": "Foo"})
+        actual.must_equal('json_class' => 'Foo')
       end
     end
 
