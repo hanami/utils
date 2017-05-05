ac7753104b1cc108b4524f53b7ae69022c14b4b5
Luca Guidi <me@lucaguidi.com>
2017-05-05 15:33:25 +0200

    Implement Utils::Hash.deep_dup (#212)

--- spec/unit/hanami/utils/hash_spec.rb
+++ spec/unit/hanami/utils/hash_spec.rb
@@ -90,6 +90,50 @@ RSpec.describe Hanami::Utils::Hash do
     end
   end
 
+  describe ".deep_dup" do
+    it "returns ::Hash" do
+      hash = described_class.deep_dup({})
+
+      expect(hash).to be_kind_of(::Hash)
+    end
+
+    it "duplicates string values" do
+      input  = { "a" => "hello" }
+      result = described_class.deep_dup(input)
+
+      result["a"] << " world"
+
+      expect(input.fetch("a")).to eq("hello")
+    end
+
+    it "duplicates array values" do
+      input  = { "a" => [1, 2, 3] }
+      result = described_class.deep_dup(input)
+
+      result["a"] << 4
+
+      expect(input.fetch("a")).to eq([1, 2, 3])
+    end
+
+    it "duplicates hash values" do
+      input  = { "a" => { "b" => 2 } }
+      result = described_class.deep_dup(input)
+
+      result["a"]["c"] = 3
+
+      expect(input.fetch("a")).to eq("b" => 2)
+    end
+
+    it "duplicates nested hashes" do
+      input  = { "a" => { "b" => { "c" => 3 } } }
+      result = described_class.deep_dup(input)
+
+      result["a"].delete("b")
+
+      expect(input).to eq("a" => { "b" => { "c" => 3 } })
+    end
+  end
+
   describe '#initialize' do
     let(:input_to_hash) do
       Class.new do
