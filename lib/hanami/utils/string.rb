f3cbf534216f057b9a985f9438c82ce7d877c528
Sean Collins <sean@cllns.com>
2017-08-09 09:49:02 -0600

    Rename 'tr' to 'transformation'

--- lib/hanami/utils/string.rb
+++ lib/hanami/utils/string.rb
@@ -134,16 +134,15 @@ module Hanami
       def self.transform(input, *transformations)
         fn = @__transformations__.fetch_or_store(transformations.hash) do
           compose do |fns|
-            transformations.each do |transformation|
-              tr, *args = transformation
-              fns << if tr.is_a?(Proc)
-                       tr
-                     elsif contain?(tr)
-                       self[*transformation]
-                     elsif input.respond_to?(tr)
-                       t(:bind, input, ->(i) { i.public_send(tr, *args) })
+            transformations.each do |transformation, *args|
+              fns << if transformation.is_a?(Proc)
+                       transformation
+                     elsif contain?(transformation)
+                       self[transformation, *args]
+                     elsif input.respond_to?(transformation)
+                       t(:bind, input, ->(i) { i.public_send(transformation, *args) })
                      else
-                       raise NoMethodError.new(%(undefined method `#{tr.inspect}' for #{input.inspect}:#{input.class}))
+                       raise NoMethodError.new(%(undefined method `#{transformation.inspect}' for #{input.inspect}:#{input.class}))
                      end
             end
           end
