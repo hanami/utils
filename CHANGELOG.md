8ebfcd03f6779349774a93bddd72b89789a0f6be
Luca Guidi <me@lucaguidi.com>
2017-10-15 13:24:01 +0200

    Prepare for v1.1.0.rc1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
 # Hanami::Utils
 Ruby core extentions and class utilities for Hanami
 
+## v1.1.0.rc1 - 2017-10-16
+
 ## v1.1.0.beta3 - 2017-10-04
 
 ## v1.1.0.beta2 - 2017-10-03
--- spec/unit/hanami/utils/version_spec.rb
+++ spec/unit/hanami/utils/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Hanami::Utils::VERSION" do
   it "exposes version" do
-    expect(Hanami::Utils::VERSION).to eq("1.1.0.beta3")
+    expect(Hanami::Utils::VERSION).to eq("1.1.0.rc1")
   end
 end
