ea64c2c5e4983a26c558bd4aa6365fcd6af5bb49
Oana Sipos <oanasipos@192-168-0-11.rdsnet.ro>
2017-12-03 14:23:35 +0200

    Extend String class to colorize strings.

--- lib/hanami/utils/string.rb
+++ lib/hanami/utils/string.rb
@@ -723,6 +723,48 @@ module Hanami
       def respond_to_missing?(m, include_private = false)
         @string.respond_to?(m, include_private)
       end
+
+      # Colorize output
+      # 8 colours available: black, red, green, yellow, blue, magenta, cyan, and gray
+      #
+      # @api private
+      # @since 1.2.beta1
+
+      def colorize(color_code)
+        "\e[#{color_code}m#{self}\e[0m"
+      end
+
+      def black
+        colorize(30)
+      end
+
+      def red
+        colorize(31)
+      end
+
+      def green
+        colorize(32)
+      end
+
+      def yellow
+        colorize(33)
+      end
+
+      def blue
+        colorize(34)
+      end
+
+      def magenta
+        colorize(35)
+      end
+
+      def cyan
+        colorize(36)
+      end
+
+      def gray
+        colorize(37)
+      end
     end
   end
 end
