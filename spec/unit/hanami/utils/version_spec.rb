54ce0fed33d625ec32965ba1d5b16693c7e86a29
Luca Guidi <me@lucaguidi.com>
2018-02-12 14:08:44 +0100

    Bump version to v1.2.0.beta1

--- spec/unit/hanami/utils/version_spec.rb
+++ spec/unit/hanami/utils/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Hanami::Utils::VERSION" do
   it "exposes version" do
-    expect(Hanami::Utils::VERSION).to eq("1.1.2")
+    expect(Hanami::Utils::VERSION).to eq("1.2.0.beta1")
   end
 end
