60c042b02bd84da2b3ab766d6ae1a859eb6606f7
Alfonso Uceda Pompa <alfonso@bebanjo.com>
2018-08-05 16:35:53 +0200

    Remove `Utils::Class.load_from_pattern!`

--- lib/hanami/utils/class.rb
+++ lib/hanami/utils/class.rb
@@ -72,57 +72,6 @@ module Hanami
         load!(name, namespace) if namespace.const_defined?(name.to_s, false)
       end
 
-      # Loads a class from the given pattern name and namespace
-      #
-      # @param pattern [String] the class name pattern
-      # @param namespace [Class, Module] the Ruby namespace where we want to perform the lookup.
-      # @return [Class, Module] the found Ruby constant.
-      #
-      # @raise [NameError] if no constant can be found.
-      #
-      # @since 0.3.1
-      #
-      # @see Hanami::Utils::String#tokenize
-      #
-      # @example
-      #   require 'hanami/utils/class'
-      #
-      #   module App
-      #     module Service
-      #       class Endpoint
-      #       end
-      #     end
-      #
-      #     class ServiceEndpoint
-      #     end
-      #   end
-      #
-      #   # basic usage
-      #   Hanami::Utils::Class.load_from_pattern!('App::Service') # => App::Service
-      #
-      #   # with explicit namespace
-      #   Hanami::Utils::Class.load_from_pattern!('Service', App) # => App::Service
-      #
-      #   # with pattern
-      #   Hanami::Utils::Class.load_from_pattern!('App::Service(::Endpoint|Endpoint)') # => App::Service::Endpoint
-      #   Hanami::Utils::Class.load_from_pattern!('App::Service(Endpoint|::Endpoint)') # => App::ServiceEndpoint
-      #
-      #   # with missing constant
-      #   Hanami::Utils::Class.load_from_pattern!('Unknown') # => raises NameError
-      def self.load_from_pattern!(pattern, namespace = Object)
-        Deprecation.new('Hanami::Utils::Class.load_from_pattern! is deprecated, please use Hanami::Utils::Class.load! instead')
-
-        tokenize(pattern) do |token|
-          begin
-            return namespace.const_get(token, false)
-          rescue NameError # rubocop:disable Lint/HandleExceptions
-          end
-        end
-
-        full_name = [(namespace == Object ? nil : namespace), pattern].compact.join('::')
-        raise NameError.new("uninitialized constant #{full_name}")
-      end
-
       # rubocop:disable Metrics/MethodLength
       def self.tokenize(pattern)
         if match = TOKENIZE_REGEXP.match(pattern) # rubocop:disable Lint/AssignmentInCondition
