d3324379cf0fd5fed6a8e4c6fd1445e9b2b97ca5
Luca Guidi <me@lucaguidi.com>
2019-02-14 12:10:42 +0100

    Allow `Hanami::Utils::FileList` to accept multiple tokens to construct a single directory path

--- lib/hanami/utils/file_list.rb
+++ lib/hanami/utils/file_list.rb
@@ -14,8 +14,42 @@ module Hanami
       # @since 0.9.0
       #
       # @see https://ruby-doc.org/core/Dir.html#method-c-glob
+      #
+      # @example simple usage
+      #   require "hanami/utils/file_list"
+      #
+      #   Hanami::Utils::FileList["spec/support/fixtures/file_list/*.rb"]
+      #   # => [
+      #     "spec/support/fixtures/file_list/a.rb",
+      #     "spec/support/fixtures/file_list/aa.rb",
+      #     "spec/support/fixtures/file_list/ab.rb"
+      #   ]
+      #
+      # @example multiple directories
+      #   require "hanami/utils/file_list"
+      #
+      #   Hanami::Utils::FileList["spec/support/fixtures/file_list/*.rb", "spec/support/fixtures/file_list/nested/*.rb"]
+      #   # => [
+      #     "spec/support/fixtures/file_list/a.rb",
+      #     "spec/support/fixtures/file_list/aa.rb",
+      #     "spec/support/fixtures/file_list/ab.rb",
+      #     "spec/support/fixtures/file_list/nested/c.rb"
+      #   ]
+      #
+      # @example token usage
+      #   require "hanami/utils/file_list"
+      #
+      #   Hanami::Utils::FileList["spec", "support", "fixtures", "file_list", "*.rb"]
+      #   # => [
+      #     "spec/support/fixtures/file_list/a.rb",
+      #     "spec/support/fixtures/file_list/aa.rb",
+      #     "spec/support/fixtures/file_list/ab.rb"
+      #   ]
       def self.[](*args)
-        Dir.glob(*args).sort!
+        directories = *args
+        directories = ::File.join(*args) unless args.any? { |a| a.include?(::File::SEPARATOR) }
+
+        Dir.glob(directories).sort!
       end
     end
   end
--- spec/unit/hanami/utils/file_list_spec.rb
+++ spec/unit/hanami/utils/file_list_spec.rb
@@ -12,5 +12,24 @@ RSpec.describe Hanami::Utils::FileList do
         "spec/support/fixtures/file_list/ab.rb"
       ]
     end
+
+    it "accepts multiple directory names" do
+      list = Hanami::Utils::FileList["spec/support/fixtures/file_list/*.rb", "spec/support/fixtures/file_list/nested/*.rb"]
+      expect(list).to eq [
+        "spec/support/fixtures/file_list/a.rb",
+        "spec/support/fixtures/file_list/aa.rb",
+        "spec/support/fixtures/file_list/ab.rb",
+        "spec/support/fixtures/file_list/nested/c.rb"
+      ]
+    end
+
+    it "finds files constructing path from directory names" do
+      list = Hanami::Utils::FileList["spec", "support", "fixtures", "file_list", "*.rb"]
+      expect(list).to eq [
+        "spec/support/fixtures/file_list/a.rb",
+        "spec/support/fixtures/file_list/aa.rb",
+        "spec/support/fixtures/file_list/ab.rb"
+      ]
+    end
   end
 end
