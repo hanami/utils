a484d901a41799b577095507a2f0ea83336ff640
Tim Riley <tim@openmonkey.com>
2021-11-09 23:40:23 +1100

    Update version spec

--- spec/unit/hanami/utils/version_spec.rb
+++ spec/unit/hanami/utils/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Utils::VERSION" do
   it "exposes version" do
-    expect(Hanami::Utils::VERSION).to eq("2.0.0.alpha2")
+    expect(Hanami::Utils::VERSION).to eq("2.0.0.alpha3")
   end
 end
