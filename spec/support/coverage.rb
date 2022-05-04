7ef19f3ad550dd0b0fc902111fa9f608a3833073
Luca Guidi <me@lucaguidi.com>
2022-05-04 09:39:00 +0200

    Fix RuboCop

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -3,7 +3,7 @@
 if ENV["CI"]
   require "simplecov"
 
-  SimpleCov.command_name ENV["SIMPLECOV_COMMAND_NAME"] || "spec:unit"
+  SimpleCov.command_name ENV.fetch("SIMPLECOV_COMMAND_NAME", "spec:unit")
   SimpleCov.start do
     add_filter(/spec/)
   end
