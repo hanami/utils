3097df93aee3a264f1bb396c1fd6c2233149c255
Peter Solnica <peter@solnica.online>
2022-11-05 07:36:51 +0100

    Fix version spec

--- spec/unit/hanami/utils/version_spec.rb
+++ spec/unit/hanami/utils/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Utils::VERSION" do
   it "exposes version" do
-    expect(Hanami::Utils::VERSION).to eq("2.0.0.beta1")
+    expect(Hanami::Utils::VERSION).to eq("2.0.0.rc1")
   end
 end
