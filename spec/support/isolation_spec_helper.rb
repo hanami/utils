76d4ac6267691f9c57b720bb9b51c54a3cc3ed20
Luca Guidi <me@lucaguidi.com>
2023-06-29 09:50:16 +0200

    Rubocop

--- spec/support/isolation_spec_helper.rb
+++ spec/support/isolation_spec_helper.rb
@@ -8,8 +8,8 @@ ENV["SIMPLECOV_COMMAND_NAME"] = "Isolation Tests PID #{$$}"
 
 $LOAD_PATH.unshift "lib"
 require "hanami/utils"
-require_relative "./rspec"
-require_relative "./coverage"
+require_relative "rspec"
+require_relative "coverage"
 
 module RSpec
   module Support
