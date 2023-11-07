4d1e0e2e4167059955d86aa9651d768288eb1c95
Luca Guidi <me@lucaguidi.com>
2023-11-07 14:49:25 +0100

    Prepare for v2.1.0.rc2

--- spec/unit/hanami/utils/version_spec.rb
+++ spec/unit/hanami/utils/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Utils::VERSION" do
   it "exposes version" do
-    expect(Hanami::Utils::VERSION).to eq("2.1.0.beta1")
+    expect(Hanami::Utils::VERSION).to eq("2.1.0.rc2")
   end
 end
