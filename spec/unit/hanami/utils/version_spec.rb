1ca56cd30452eb5fc71aafd8b8adc6f4ddff1a78
Tim Riley <tim@riley.id.au>
2024-11-05 19:26:37 +1100

    Prepare for v2.2.0

--- spec/unit/hanami/utils/version_spec.rb
+++ spec/unit/hanami/utils/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::Utils::VERSION" do
   it "exposes version" do
-    expect(Hanami::Utils::VERSION).to eq("2.2.0.rc1")
+    expect(Hanami::Utils::VERSION).to eq("2.2.0")
   end
 end
